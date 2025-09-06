488808ba4e1f5d0f8c03cbf805e84d038642ca25
KRISH KUMAR SINGH <ks092713@gmail.com>
2025-09-06 08:38:37 +0000

    feat: Replace MapComponent with SmartChallan in Dash_2 for enhanced functionality

--- traffic-flow/src/pages/Dash_2.jsx
+++ traffic-flow/src/pages/Dash_2.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import MapComponent from './Traffic_update'
+
 import TrafficDashboard from '../components/ui/Dashboard'
+import SmartChallan from './SmartChallan'
 function Dash_2() {
   return (
     <div className='h-full w-full p-3 bg-[#0A0D14]'>
       {/* <Navbar_2/> */}
       <TrafficDashboard />
-      <MapComponent/>
+      <SmartChallan/>
 
     </div>
   )
